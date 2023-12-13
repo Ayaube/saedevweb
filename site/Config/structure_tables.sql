44d56557cecf8c736c136307a21395b1f7e1b486
Ayaube <ayoub.lamchichi.info@gmail.com>
2023-12-13 14:12:51 +0100

    dernier script

--- site/Config/structure_tables.sql
+++ site/Config/structure_tables.sql
@@ -1,67 +1,66 @@
 -- Table: Joueur
 CREATE TABLE Joueur(
-                       id_joueur     INTEGER PRIMARY KEY AUTOINCREMENT,
-                        username TEXT NOT NULL ,
-                       email         TEXT NOT NULL,
-                       passw_hash    TEXT NOT NULL,
-                       date_creation TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP
+                       id_joueur     INT AUTO_INCREMENT PRIMARY KEY,
+                       username      VARCHAR(255) NOT NULL,
+                       email         VARCHAR(255) NOT NULL,
+                       passw_hash    VARCHAR(255) NOT NULL,
+                       date_creation TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
 
 -- Table: Ennemis
 CREATE TABLE Ennemis(
-                        id_ennemi       INTEGER PRIMARY KEY AUTOINCREMENT,
-                        nom_ennemi      TEXT NOT NULL,
-                        pv              INTEGER NOT NULL,
-                        vitesse         INTEGER NOT NULL,
-                        gain            INTEGER NOT NULL,
+                        id_ennemi       INT AUTO_INCREMENT PRIMARY KEY,
+                        nom_ennemi      VARCHAR(255) NOT NULL,
+                        pv              INT NOT NULL,
+                        vitesse         INT NOT NULL,
+                        gain            INT NOT NULL,
                         desc            TEXT NOT NULL,
-                        id_proprietaire INTEGER NOT NULL,
-                        id_joueur       INTEGER NOT NULL,
-                        FOREIGN KEY (id_joueur) REFERENCES Joueur(id_joueur)
+                        id_proprietaire INT NOT NULL,
+                        FOREIGN KEY (id_proprietaire) REFERENCES Joueur(id_joueur)
 );
+
 
 -- Table: Map
 CREATE TABLE Map(
-                    id_map          INTEGER PRIMARY KEY AUTOINCREMENT,
-                    nom_map         TEXT NOT NULL,
+                    id_map          INT AUTO_INCREMENT PRIMARY KEY,
+                    nom_map         VARCHAR(255) NOT NULL,
                     matrix          TEXT NOT NULL,
-                    id_proprietaire INTEGER NOT NULL,
-                    id_joueur       INTEGER NOT NULL,
-                    FOREIGN KEY (id_joueur) REFERENCES Joueur(id_joueur)
+                    id_proprietaire INT NOT NULL,
+                    FOREIGN KEY (id_proprietaire) REFERENCES Joueur(id_joueur)
 );
+
 
 -- Table: Tourelle
 CREATE TABLE Tourelle(
-                         id_tourelle     INTEGER PRIMARY KEY AUTOINCREMENT,
-                         nom_tourelle    TEXT NOT NULL,
-                         date_creation   TEXT NOT NULL,
-                         id_proprietaire INTEGER NOT NULL,
-                         prix_ram        INTEGER NOT NULL,
-                         prix_flops      INTEGER NOT NULL,
-                         id_joueur       INTEGER NOT NULL,
-                         FOREIGN KEY (id_joueur) REFERENCES Joueur(id_joueur)
+                         id_tourelle     INT AUTO_INCREMENT PRIMARY KEY,
+                         nom_tourelle    VARCHAR(255) NOT NULL,
+                         date_creation   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                         id_proprietaire INT NOT NULL,
+                         prix_ram        INT NOT NULL,
+                         prix_flops      INT NOT NULL,
+                         FOREIGN KEY (id_proprietaire) REFERENCES Joueur(id_joueur)
 );
-
 
 
 -- Table: Partie
 CREATE TABLE Partie(
-                       id_partie INTEGER PRIMARY KEY AUTOINCREMENT,
-                       id_joueur INTEGER NOT NULL,
-                       map INTEGER NOT NULL,
-                       vague_atteinte INTEGER NOT NULL,
-                       ennemis_tue INTEGER NOT NULL,
-                       argent_gagne INTEGER NOT NULL,
-                       id_joueur_Joue INTEGER NOT NULL,
-                       id_map INTEGER NOT NULL,
-                       FOREIGN KEY (id_joueur_Joue) REFERENCES Joueur(id_joueur),
+                       id_partie      INT AUTO_INCREMENT PRIMARY KEY,
+                       id_joueur      INT NOT NULL,
+                       map            INT NOT NULL,
+                       vague_atteinte INT NOT NULL,
+                       ennemis_tue    INT NOT NULL,
+                       argent_gagne   INT NOT NULL,
+                       id_map         INT NOT NULL,
+                       FOREIGN KEY (id_joueur) REFERENCES Joueur(id_joueur),
                        FOREIGN KEY (id_map) REFERENCES Map(id_map)
 );
 
+
 -- Table: Detient
 CREATE TABLE Detient(
-                        id_joueur INTEGER NOT NULL,
-                        id_tourelle INTEGER NOT NULL,
+                        id_joueur   INT NOT NULL,
+                        id_tourelle INT NOT NULL,
                         PRIMARY KEY (id_joueur, id_tourelle),
                         FOREIGN KEY (id_joueur) REFERENCES Joueur(id_joueur),
                         FOREIGN KEY (id_tourelle) REFERENCES Tourelle(id_tourelle)
