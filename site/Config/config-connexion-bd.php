14f53b0aef3960f4350d4d1462ae71fde422ca77
Ayaube <ayoub.lamchichi.info@gmail.com>
2023-12-13 14:27:03 +0100

    config sql

--- site/Config/config-connexion-bd.php
+++ site/Config/config-connexion-bd.php
@@ -1,22 +1,9 @@
 <?php
-
-// Fichier config-connexion-bd.php
-
-class DatabaseConfig {
-    private $servername = "ayoublsae.mysql.db";
-    private $username = "ayoublsae";
-    private $password = "Saedevweb2023";
-    private $dbname = "ayoublsae";
-
-    public function connect() {
-        $conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
-
-        if ($conn->connect_error) {
-            die("La connexion a échoué : " . $conn->connect_error);
-        }
-
-        return $conn;
-    }
-}
-
+// config-connexion-bd.php
+$dbConfig = [
+    'servername' => 'localhost',
+    'username' => 'ayoub',
+    'password' => 'Ayoubsae2023',
+    'dbname' => 'saebd'
+];
 ?>
---
+++ site/Config/test_connexion.php
@@ -0,0 +1,22 @@
+<?php
+// Inclure le fichier de configuration
+require 'config-connexion-bd.php';
+
+
+$servername = $dbConfig['servername'];
+$username = $dbConfig['username'];
+$password = $dbConfig['password'];
+$dbname = $dbConfig['dbname'];
+
+// Créer une connexion
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Vérifier la connexion
+if ($conn->connect_error) {
+    die("La connexion a échoué : " . $conn->connect_error);
+}
+echo "Connecté avec succès à la base de données";
+
+// Fermer la connexion
+$conn->close();
+?>
