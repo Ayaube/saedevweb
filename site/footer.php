e2ad42ca96abfc856eb160b1c1f2a7cecc3e40a4
Ayaube <122544136+Ayaube@users.noreply.github.com>
2023-12-14 11:30:56 +0100

    - création des différentes pages - mise en forme - debut d'une charte graphique

--- site/footer.php
+++ site/footer.php
@@ -1,4 +1,4 @@
-<div class="footer bg-light mt-4">
+<div class="footer mt-4">
     <div class="container text-center py-3">
         <p>Site créé par Lucas LIGER, Ayoub LAMCHICHI et Horeb SILVA dans le cadre de la SAE</p>
         <p>© 2023 IUT de Montreuil. Tous droits réservés.</p>
