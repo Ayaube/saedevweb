85d034f86a1c4d39511cf691c8fe8f862c2f76c2
Horeb SILVA <horeb.silva241004@gmail.com>
2024-01-25 18:54:37 +0100

    update

--- site/modules/a-propos/equipe/horeb/view-horeb.php
+++ site/modules/a-propos/equipe/horeb/view-horeb.php
@@ -9,15 +9,15 @@ class HorebView {
         include_once "header.php";
         ?>
         <div class="card">
-            <div class="card-body bg-light ">
-                <h1 class ="text-center"> Horeb, 19 ans, passionné d'informatique et de MMA</h1>
-                <p class ="text-center">Je suis actuellement étudiant en deuxième année de Bachelor Universitaire de Technologie (BUT) informatique à l'IUT de Montreuil. Parallèlement à ma formation, je suis en alternance chez SIKIWIS, où je travaille en tant que développeur.</p>
-                <p class ="text-center"> A coté de tout cela, je suis champion du monde poids lourd à l'UFC depuis decembre 2023. J'ai obtenu la ceinture après avoir mis KO avec ma claque devastatrice l'ancien champion, Jon Jones en seulement 1min30 de combat</p>
-            </div>
-            <div class="text-center">
+            <div class="text-center card-body bg-transparent border border-black">
+                <h1> Horeb, 19 ans, passionné d'informatique et de MMA</h1>
+                <p>Je suis actuellement étudiant en deuxième année de Bachelor Universitaire de Technologie (BUT) informatique à l'IUT de Montreuil. Parallèlement à ma formation, je suis en alternance chez SIKIWIS, où je travaille en tant que développeur.</p>
+                <p> A coté de tout cela, je suis champion du monde poids lourd à l'UFC depuis decembre 2023. J'ai obtenu la ceinture après avoir mis KO avec ma claque devastatrice l'ancien champion, Jon Jones en seulement 1min30 de combat</p>
                 <a href="https://www.instagram.com/hbs_470" target="_blank"><img src="images/logo/insta.png"></a>
                 <a href="https://twitter.com/hbs_470" target="_blank"><img src="images/logo/x.png"></a>
             </div>
+
+
 
         </div>
 
--- site/modules/a-propos/equipe/lucas/view-lucas.php
+++ site/modules/a-propos/equipe/lucas/view-lucas.php
@@ -9,16 +9,13 @@ class LucasView {
         include_once "header.php";
         ?>
         <div class="card">
-            <div class="card-body bg-light ">
-                <h1 class ="text-center"> Lucas, passionné de développement et sport</h1>
-                <p class ="text-center">Actuellement, je poursuis mes études en deuxième année du Bachelor Universitaire de Technologie en informatique à l'IUT de Montreuil. En complément de mes études, je suis également en formation en alternance chez Credit Agricole.</p>
-                <p class ="text-center"> J'devais percer au foot mais vas-y ligament tu connais</p>
-            </div>
-            <div class="text-center">
+            <div class="text-center card-body bg-transparent border border-black">
+                <h1> Lucas, passionné de développement et sport</h1>
+                <p>Actuellement, je poursuis mes études en deuxième année du Bachelor Universitaire de Technologie en informatique à l'IUT de Montreuil. En complément de mes études, je suis également en formation en alternance chez Credit Agricole.</p>
+                <p> J'devais percer au foot mais vas-y ligament tu connais</p>
                 <a href="https://www.instagram.com/#" target="_blank"><img src="images/logo/insta.png"></a>
                 <a href="https://twitter.com/#" target="_blank"><img src="images/logo/x.png"></a>
             </div>
-
         </div>
 
         <?php
