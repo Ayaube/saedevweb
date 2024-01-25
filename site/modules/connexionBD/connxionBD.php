485c30978eaaa4298a2f48b922bff5583a21ec0f
Ayaube <ayoub.lamchichi.info@gmail.com>
2024-01-25 19:46:51 +0100

    - Correction de la connexion

--- site/modules/connexionBD/connxionBD.php
+++ site/modules/connexionBD/connxionBD.php
@@ -10,7 +10,7 @@ if (!defined('MY_APP')) {
         }
         public function connexion() {
             try {
-                self::$bdd = new PDO('mysql:host=database-etudiants.iut.univ-paris8.fr;dbname=dutinfopw201676', 'dutinfopw201676', 'hutunyzu');            
+                self::$bdd = new PDO('mysql:host=ayoublsae.mysql.db;dbname=ayoublsae', 'ayoublsae', 'Motdep4sse');
                 self::$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch (PDOException $e) {
                 die('Erreur de connexion à la base de données : ' . $e->getMessage());
