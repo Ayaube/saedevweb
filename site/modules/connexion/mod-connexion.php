6e2b176d167166d2763cf5066855ac3e954e50e0
Lucass-Dev <liger.lucas33@gmail.com>
2024-01-26 12:39:27 +0100

    finilisation  protection faille csrf

--- site/modules/connexion/mod-connexion.php
+++ site/modules/connexion/mod-connexion.php
@@ -6,22 +6,7 @@ require_once './modules/connexionBD/connxionBD.php';
 
 class ConnexionModel extends Connexion {
 
-    function checkCSRFToken(){
-        if(isset($_POST['csrf_token'])) {
-            $user_token = $_POST['csrf_token'];
-            if($user_token === $_SESSION['csrf_token']) {
-                echo "ok";
-                return true;
-            } else {
-                echo "Erreur : Jeton CSRF non valide.";
-                return false;
-            }
-            unset($_SESSION['csrf_token']);
-        } else {
-            echo "Erreur : Jeton CSRF manquant dans la requête.";
-            return false;
-        }
-    }
+
 
     public function checkCredentials($username, $password) {
         $stmt = self::$bdd->prepare('SELECT username, passw_hash FROM Joueur WHERE username = :username');
--- site/modules/connexionBD/connxionBD.php
+++ site/modules/connexionBD/connxionBD.php
@@ -16,7 +16,35 @@ if (!defined('MY_APP')) {
                 die('Erreur de connexion à la base de données : ' . $e->getMessage());
             }
         }
-        
+
+        function checkCSRFToken(){
+            if(isset($_POST['csrf_token'])) {
+                $user_token = $_POST['csrf_token'];
+                if($user_token === $_SESSION['csrf_token']) {
+                    return true;
+                } else {
+                    echo "Erreur : Jeton CSRF non valide.";
+                    return false;
+                }
+
+            }else if(isset($_GET['csrf_token'])) {
+                $user_token = $_GET['csrf_token'];
+                if($user_token === $_SESSION['csrf_token']) {
+                    return true;
+                } else {
+                    echo "Erreur : Jeton CSRF non valide.";
+                    return false;
+                }
+
+            } else {
+                echo "Erreur : Jeton CSRF manquant dans la requête.";
+                return false;
+            }
+        }
+
+        static function generateCSRFToken() {
+            return bin2hex(random_bytes(32));
+        }
     }
     
 ?>
