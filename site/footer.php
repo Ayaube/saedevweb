08c90186a1c21a81535f36919cd8be1cc803da97
Horeb SILVA <horeb.silva241004@gmail.com>
2024-01-26 12:53:12 +0100

    Update footer.php

--- site/footer.php
+++ site/footer.php
@@ -2,6 +2,17 @@
     <div class="container text-center py-3">
         <p>Site créé par Lucas LIGER, Ayoub LAMCHICHI et Horeb SILVA dans le cadre de la SAE</p>
         <p>© 2024 IUT de Montreuil. Tous droits réservés.</p>
-        <p><a href="index.php?module=feedback" class="footer-feedback-link">Soumettre un feedback</a></p>
+        <?php
+            if (isset($_SESSION['role'])&&$_SESSION['role']!='admin' || !isset($_SESSION['role'])) {
+                ?>
+                <p><a href="index.php?module=feedback" class="footer-feedback-link">Soumettre un feedback</a></p>
+                <!--        <p><button onclick="window.location.href='index.php?module=feedback';" class="btn btn-light">Soumettre un feedback</button></p>-->
+                <?php
+            }
+
+        ?>
+
     </div>
+
+
 </div>
