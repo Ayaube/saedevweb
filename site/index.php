3b1cfd963fdbc071ed11f7e7c8ef1953d0aa6b3e
Horeb SILVA <horeb.silva241004@gmail.com>
2024-01-26 17:21:33 +0100

    Update index.php

--- site/index.php
+++ site/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tower Defense</title>
+    <title>Files Attack !</title>
     <link rel="stylesheet" href="./css/bootstrap.css">
     <link rel="stylesheet" href="./css/style.css">
     <link rel="icon" href="./images/tower.png" type="image/x-icon">
